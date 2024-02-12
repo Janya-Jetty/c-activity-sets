265c7f5792486b7e0d19d4043fb7138ea468de8f
Janya Jetty <janyaj.bsc23@rvu.edu.in>
2024-02-12 14:15:01 +0530

    test

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,7 +1,10 @@
 // 9. Write a C program to find the square root of a number
 
 #include <stdio.h>
-#include <math.h>
+
+float input();
+float square_root(float n);
+void output(float n, float sqrroot);
 
 int main() 
 {
