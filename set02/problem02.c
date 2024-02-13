8a96e76e8f9a5562c9cf6ab2785d26db006f3ed8
Janya Jetty <janyaj.bsc23@rvu.edu.in>
2024-02-13 14:37:56 +0530

    test2

---
+++ set02/problem02.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int input_side();
+int check_scalene(int a, int b, int c);
+void output(int a, int b, int c, int isscalene);
+
+int main() 
+{
+    int side1, side2, side3, isscalene;
+    side1 = input_side();
+    side2 = input_side();
+    side3 = input_side();
+    isscalene = check_scalene(side1, side2, side3);
+    output(side1, side2, side3, isscalene);
+    return 0;
+}
+
+int input_side() 
+{
+    int side;
+    printf("Enter the length of a side: ");
+    scanf("%d", &side);
+    return side;
+}
+
+int check_scalene(int a, int b, int c) 
+{
+    if (a != b && b != c && a != c) 
+    {
+        return 1;
+    } 
+    else 
+    {
+        return 0;
+    }
+}
+
+void output(int a, int b, int c, int isscalene) 
+{
+    printf("The triangle with sides %d, %d and %d is ", a, b, c);
+    if (isscalene) 
+    {
+        printf("a scalene triangle.\n");
+    } 
+    else 
+    {
+        printf("not a scalene triangle.\n");
+    }
+}
