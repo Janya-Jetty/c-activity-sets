726428976f1037a3f42a80143ae8c76e1068b5a2
Janya Jetty <janyaj.bsc23@rvu.edu.in>
2024-02-13 14:53:05 +0530

    test2

--- set02/problem02.c
+++ set02/problem02.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
 int input_side();
-int check_scalene(int a, int b, int c);
-void output(int a, int b, int c, int isscalene);
+int check_scalene(int a,int b,int c);
+void output(int a,int b,int c,int isscalene);
 
 int main() 
 {
-    int side1, side2, side3, isscalene;
-    side1 = input_side();
-    side2 = input_side();
-    side3 = input_side();
-    isscalene = check_scalene(side1, side2, side3);
-    output(side1, side2, side3, isscalene);
+    int a,b,c,isscalene;
+    a = input_side();
+    b = input_side();
+    c = input_side();
+    isscalene = check_scalene(a,b,c);
+    output(a,b,c,isscalene);
     return 0;
 }
 
 int input_side() 
 {
-    int side;
+    int n;
     printf("Enter the length of a side: ");
-    scanf("%d", &side);
-    return side;
+    scanf("%d", &n);
+    return n;
 }
 
-int check_scalene(int a, int b, int c) 
+int check_scalene(int a,int b,int c) 
 {
-    if (a != b && b != c && a != c) 
+    if (a!=b && b!=c && a!=c) 
     {
         return 1;
     } 
@@ -35,7 +35,7 @@ int check_scalene(int a, int b, int c)
     }
 }
 
-void output(int a, int b, int c, int isscalene) 
+void output(int a,int b,int c,int isscalene) 
 {
     printf("The triangle with sides %d, %d and %d is ", a, b, c);
     if (isscalene) 
